3dab03190ce6c820014c7f1484bb88f6530a2d4f
tyngodz160604 <tyn844657@gmail.com>
2025-10-18 17:42:35 +0700

    Hoàn thành CRUD API backend với MONGODB

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,15 +1,47 @@
-let users = [];
+const User = require('../models/User');
 
-exports.getUsers = (req, res) => {
-  res.json(users);
+
+// [GET] /users
+exports.getUsers = async (req, res) => {
+  try {
+    const users = await User.find();
+    res.json(users);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
 };
 
-exports.createUser = (req, res) => {
-  const newUser = {
-    id: users.length + 1,
-    name: req.body.name,
-    email: req.body.email
-  };
-  users.push(newUser);
-  res.status(201).json(newUser);
+// [POST] /users
+exports.createUser = async (req, res) => {
+  try {
+    const newUser = new User({
+      name: req.body.name,
+      email: req.body.email
+    });
+    await newUser.save();
+    res.status(201).json(newUser);
+  } catch (err) {
+    res.status(400).json({ message: err.message });
+  }
+};
+
+// [PUT] /users/:id
+exports.updateUser = async (req, res) => {
+  try {
+    const updatedUser = await User.findByIdAndUpdate(req.params.id, req.body, { new: true });
+    if (!updatedUser) return res.status(404).json({ message: 'User not found' });
+    res.json(updatedUser);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+};
+
+// [DELETE] /users/:id
+exports.deleteUser = async (req, res) => {
+  try {
+    await User.findByIdAndDelete(req.params.id);
+    res.json({ message: 'User deleted' });
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
 };
